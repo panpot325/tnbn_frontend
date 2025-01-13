812a0b6ffcdcb59ad1cfa16ad7080f7edd59ba89
kyopen <kyopen@asahinet.jp>
2025-01-13 12:25:44 +0900

    BgColorの設定

--- share/BgColor.cs
+++ share/BgColor.cs
@@ -6,17 +6,17 @@ namespace WorkDataStudio.share;
 /// BgColor
 /// </summary>
 public static class BgColor {
-    public static readonly Color CLEARED = Color.FromArgb(0, 255, 255, 255); //透明
+    public static readonly Color CLEARED = Color.FromArgb(0, 0xFF, 0xFF, 0xFF); //透明
     public static readonly Color DEFAULT = Color.FromArgb(255, 255, 255); //Default
-    public static readonly Color UPDATED = Color.FromArgb(192, 255, 192); //更新対象データ
-    public static readonly Color DELETED = Color.FromArgb(255, 255, 192); //削除対象データ
-    public static readonly Color INVALID = Color.FromArgb(255, 192, 192); //入力エラーまたはキー重複
-    public static readonly Color SELECTED = Color.FromArgb(192, 255, 255); //選択行
-    public static readonly Color DISABLED = Color.FromArgb(224, 224, 224); //入力不可
-    public static readonly Color S_GUNWALE_N = Color.FromArgb(255, 244, 192); //S舷を作成しない
-    public static readonly Color S_GUNWALE_E = Color.FromArgb(255, 192, 255); //S舷を作成（東基準）
-    public static readonly Color S_GUNWALE_W = Color.FromArgb(255, 255, 255); //S舷を作成（西基準）
-    public static readonly Color P_GUNWALE_N = Color.FromArgb(255, 255, 255); //P舷を作成しない
-    public static readonly Color P_GUNWALE_E = Color.FromArgb(192, 192, 255); //P舷を作成（東基準）
-    public static readonly Color P_GUNWALE_W = Color.FromArgb(255, 192, 128); //P舷を作成（西基準）
+    public static readonly Color UPDATED = Color.FromArgb(0xC0, 0xFF, 0xC0); //更新対象データ
+    public static readonly Color DELETED = Color.FromArgb(0xFF, 0xFF, 0xC0); //削除対象データ
+    public static readonly Color INVALID = Color.FromArgb(0xFF, 0xC0, 0xC0); //入力エラーまたはキー重複
+    public static readonly Color SELECTED = Color.FromArgb(0xC0, 0xFF, 0xFF); //選択行
+    public static readonly Color DISABLED = Color.FromArgb(0xE0, 0xE0, 0xE0); //入力不可
+    public static readonly Color S_GUNWALE_N = Color.FromArgb(0xFF, 0xE0, 0xC0); //S舷を作成しない
+    public static readonly Color S_GUNWALE_E = Color.FromArgb(0xFF, 0xC0, 0xFF); //S舷を作成（東基準）
+    public static readonly Color S_GUNWALE_W = Color.FromArgb(0xFF, 0xFF, 0xFF); //S舷を作成（西基準）
+    public static readonly Color P_GUNWALE_N = Color.FromArgb(0xFF, 0xFF, 0xFF); //P舷を作成しない
+    public static readonly Color P_GUNWALE_E = Color.FromArgb(0xC0, 0xC0, 0xFF); //P舷を作成（東基準）
+    public static readonly Color P_GUNWALE_W = Color.FromArgb(0xFF, 0xC0, 0x80); //P舷を作成（西基準）
 }
